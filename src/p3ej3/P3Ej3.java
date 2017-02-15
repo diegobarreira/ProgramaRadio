532e6cb692309defdb5341fcc6a72be9c810022a
Fran <Fran@PC-Fran>
2017-02-15 20:28:23 +0100

    V2.1

--- src/p3ej3/P3Ej3.java
+++ src/p3ej3/P3Ej3.java
@@ -15,24 +15,36 @@ public class P3Ej3 {
     private static LocalTime inicioPrograma = LocalTime.of(0, 0);
     private static long duracionRestanteMinutos = 1440;
 
-    private static enum MENSAJE {
-        TITULO, LOCUTOR, TIEMPO
-    }
-
     public static void main(String[] args) {
         ArrayList<ProgramaRadio> misProgramas = new ArrayList<>();
 
         long duracion = 0;
         ProgramaRadio radioFM;
 
-        while (duracionRestanteMinutos > 0) {
+        do {
+            System.out.println("El próximo programa de radio comenzará a las " + inicioPrograma);
+            System.out.println("Duracion restante para la emisión " + duracionRestanteMinutos + " minutos");
             if (duracionRestanteMinutos > DURACION_MINIMA_MINUTOS) {
-                String titulo = insertarDato(MENSAJE.TITULO);
-                String locutor = insertarDato(MENSAJE.LOCUTOR);
+                String titulo = JOptionPane.showInputDialog("Inserte el Título del programa");
+                String locutor = JOptionPane.showInputDialog("Inserte el nombre del locutor del programa");
+                boolean repetir = false;
                 do {
-                    System.out.println("Duración mínima es de 60 minutos y la máxima es de 1440");
-                    duracion = Long.parseLong(insertarDato(MENSAJE.TIEMPO));
-                } while (duracion < DURACION_MINIMA_MINUTOS || duracion > DURACION_MAXIMA_MINUTOS);
+                    repetir = false;
+                    try {
+                        duracion = Long.parseLong(JOptionPane.showInputDialog("Inserte la duración en minutos"));
+                        if (duracion < DURACION_MINIMA_MINUTOS) {
+                            System.err.println("Debe durar más de " + DURACION_MINIMA_MINUTOS + " minutos");
+                            repetir = true;
+                        }
+                        if (duracion > DURACION_MAXIMA_MINUTOS) {
+                            System.err.println("Debe durar menos de " + DURACION_MAXIMA_MINUTOS + " minutos");
+                            repetir = true;
+                        }
+                    } catch (NumberFormatException e) {
+                        System.err.println("Debe introducir números");
+                        repetir = true;
+                    }
+                } while (repetir);
                 duracionRestanteMinutos -= duracion;
                 radioFM = new ProgramaRadio(titulo, locutor, duracion, inicioPrograma);
                 inicioPrograma = inicioPrograma.plusMinutes(duracion);
@@ -41,33 +53,11 @@ public class P3Ej3 {
                 radioFM = new ProgramaRadio("Publicidad", "Sin locutor", duracionRestanteMinutos, inicioPrograma);
                 misProgramas.add(radioFM);
             }
-        }
+        } while (duracionRestanteMinutos > 0);
 
         for (ProgramaRadio radio : misProgramas) {
             System.out.println(radio);
         }
     }
 
-    public static String insertarDato(MENSAJE msj) {
-        String dato = "";
-        boolean error = false;
-        do {
-            error = false;
-            if (msj.TITULO == MENSAJE.TITULO) {
-                dato = JOptionPane.showInputDialog("Inserte el Título del programa");
-            }
-            if (msj.LOCUTOR == MENSAJE.LOCUTOR) {
-                dato = JOptionPane.showInputDialog("Inserte el Locutor del programa");
-            }
-            try {
-                if (msj.TIEMPO == MENSAJE.TIEMPO) {
-                    dato = JOptionPane.showInputDialog("Inserte las horas de duración del programa");
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Error en el formato numérico");
-                error = true;
-            }
-        } while (error);
-        return dato;
-    }
 }
